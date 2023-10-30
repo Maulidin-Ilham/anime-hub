b157dc8c79404649767f119e023587d262bf68c7
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-10-30 22:03:18 +0700

    add upcoming anime in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -4,24 +4,56 @@ import Image from "next/image";
 
 const page = async () => {
   const data = await fetchRandom();
-  const anime = data.data;
+  const animeRandom = data.data;
+  const randomId = Math.floor(Math.random() * 25);
+  const anime = animeRandom[randomId];
 
   return (
     <div className="px-4 flex flex-col  ">
-      <div className="w-full border-2 border-gray-200 rounded-md h-[270px] relative">
-        <div className="w-full h-full">
-          <img
-            src={anime.trailer.images.maximum_image_url}
-            alt={anime.title}
-            className="w-full h-full object-cover"
-          />
+      <div className="w-full border-2 border-gray-200  h-[270px] relative">
+        <div className="w-full h-full ">
+          {anime.trailer.images.maximum_image_url !== null ? (
+            <img
+              src={anime.trailer.images.maximum_image_url}
+              alt={anime.title}
+              className="w-full h-full object-cover  "
+            />
+          ) : (
+            <img
+              src={anime.images.webp.large_image_url}
+              alt={anime.title}
+              className="w-full h-full object-cover  "
+            />
+          )}
         </div>
-        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-30% to-60% from-black ">
-          <div className="flex flex-col justify-center items-start mt-16 px-4 space-y-2">
-            <h1 className="text-white text-xl font-bold ">{anime.title}</h1>
-            <h1 className="text-white   ">{anime.type}</h1>
-            <h1 className="text-white   ">{anime.episodes}</h1>
-            <h1 className="text-white   bg-gray-300 px-2">{anime.status}</h1>
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r  from-black ">
+          <div className="flex flex-col justify-center items-start mt-8 px-4 ">
+            <h1 className="text-white text-2xl font-bold ">{anime.title}</h1>
+
+            <div className="flex flex-row space-x-2 mt-3  ">
+              {anime.genres.slice(0, 3).map((genre, index) => (
+                <h1
+                  key={index}
+                  className="text-white text-base bg-blue-400 px-2 rounded"
+                >
+                  {genre.name}
+                </h1>
+              ))}
+            </div>
+            <h1 className="text-white mt-2">
+              {anime.synopsis !== null && anime.synopsis.length > 70
+                ? anime.synopsis.substring(0, 70) + "..." // Display the first 100 characters and add an ellipsis
+                : anime.synopsis}
+            </h1>
+
+            {anime.season !== null && anime.year !== null ? (
+              <h1 className="text-white font-semibold mt-2 text-lg capitalize">
+                {anime.season} {anime.year}
+              </h1>
+            ) : (
+              <h1 className="text-white text-lg font-semibold mt-2">Soon</h1>
+            )}
+            {/* {anime.season} {anime.year} */}
           </div>
         </div>
       </div>
--- app/utils/fetchRandom.jsx
+++ app/utils/fetchRandom.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 export const fetchRandom = async () => {
-  const randomId = Math.random() * 1000;
-  const res = await fetch(`https://api.jikan.moe/v4/anime/1`);
+  const randomId = Math.random() * 100;
+  // const res = await fetch(`https://api.jikan.moe/v4/random/anime`);
+  const res = await fetch(`https://api.jikan.moe/v4/seasons/upcoming`);
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
