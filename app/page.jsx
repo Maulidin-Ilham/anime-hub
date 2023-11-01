b87545dc2c27b32a31abc2f50b7e96d8c2a1f97e
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-01 09:21:10 +0700

    fixing error spoofer.js

--- app/page.jsx
+++ app/page.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { fetchRandom } from "./utils/fetchRandom";
 import Image from "next/image";
 import Hero from "./components/Hero";
+import { Carousel } from "@trendyol-js/react-carousel";
 
 const page = async () => {
   const data = await fetchRandom();
   const animeRandom = data.data;
   const randomId = Math.floor(Math.random() * 25);
   const anime = animeRandom[randomId];
-
+  // console.log(animeRandom);
   return (
     <div className="px-4 flex flex-col  ">
       <Hero anime={anime} />
+
       <div>hello</div>
     </div>
   );
