9d734bfd6f7dbc48af077d383894e89747f22266
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-02 11:18:38 +0700

    add trailer in detail page

--- app/anime/[id]/page.jsx
+++ app/anime/[id]/page.jsx
@@ -1,19 +1,20 @@
+"use client";
 import { fetchAnimeDetail } from "@/app/utils/fetchAnimeDetail";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import YouTube from "react-youtube";
 
 const page = async ({ params }) => {
   const mal_id = params.id;
   const animeDetails = await fetchAnimeDetail(mal_id);
   const details = animeDetails.data;
-  console.log(mal_id);
-  console.log(details);
+
   return (
-    <div className="p-4   ">
+    <div className="p-4 w-full ">
       <h1 className="text-xl font-bold">Details Page</h1>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col mt-4">
         <h1 className="text-lg font-semibold mb-2">{details.title}</h1>
         <div className="flex flex-row space-x-7 ">
           <Image
@@ -22,14 +23,14 @@ const page = async ({ params }) => {
             height={200}
             alt={details.title}
           />
-          <div className="flex flex-row space-x-3">
-            <div className="flex flex-col  mr-8">
+          <div className="flex md:flex-row md:space-x-3  flex-col">
+            <div className="flex flex-col  ">
               <h1>Score</h1>
               <h1 className="font-bold text-lg">{details.score}</h1>
-              <h1>{details.scored_by.toLocaleString("en-US")} </h1>
+              <h1 className="">{details.scored_by.toLocaleString("en-US")} </h1>
               <h1>users</h1>
             </div>
-            <h1>
+            <h1 className="mt-3 md:mt-0">
               Ranked # <span className="font-bold">{details.rank}</span>
             </h1>
             <h1>
@@ -40,6 +41,18 @@ const page = async ({ params }) => {
               Members <span className="font-bold">{details.members}</span>
             </h1>
           </div>
+        </div>
+        {/* video */}
+        <div className="mt-6">
+          <h1 className="text-lg font-semibold">Trailer</h1>
+          {details.trailer.youtube_id !== null ? (
+            <YouTube
+              videoId={details.trailer.youtube_id}
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <h1>Video tidak tersedia</h1>
+          )}
         </div>
       </div>
     </div>
